a7d61d3166e52f4257cbd2eed41fdbcfe833b5a1
readberg <wayreadberg@gmail.com>
2023-01-20 11:45:44 +0300

    feat: add basic stucture of app

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            Genfi
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+        <nav className='nav'>
+        <ul className='nav-list'>
+          <li className='nav-item'><a href="#" className='nav-link'>Profile</a></li>
+          <li className='nav-item'><a href="#" className='nav-link'>Feed</a></li>
+          <li className='nav-item'><a href="#" className='nav-link'>Messages</a></li>
+          <li className='nav-item'><a href="#" className='nav-link'>Music</a></li>
+          <li className='nav-item'><a href="#" className='nav-link'>Settings</a></li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Nav;
