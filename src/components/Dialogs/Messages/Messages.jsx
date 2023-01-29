fe14a6f12205475424d7dc3abfa0216ca8f5895a
readberg <wayreadberg@gmail.com>
2023-01-29 08:09:49 +0300

    feat: add ref on messages

--- src/components/Dialogs/Messages/Messages.jsx
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -1,12 +1,26 @@
 import Message from "./Message/Message";
 import c from "./Messages.module.css";
+import React from "react";
 
 function Messages(props) {
   let messagesElements = props.messages.map((m) => (
     <Message message={m.message} />
   ));
 
-  return <div className={c.messages}>{messagesElements}</div>;
+  let newMessageElement = React.createRef();
+
+  let newMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text);
+  }
+
+  return <div className={c.messages}>
+    {messagesElements}
+    <div className={c.addMessage}>
+      <textarea ref={newMessageElement} className={c.messageText}></textarea>
+      <button className={c.messageBtn} onClick={newMessage}>Send</button>
+    </div>
+    </div>;
 }
 
 export default Messages;
